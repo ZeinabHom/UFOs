a4223759d73197c65eef031341da4f442a23ccef
ZeinabHom <91231253+ZeinabHom@users.noreply.github.com>
2022-01-22 18:53:21 -0500

    review function - add attr for "id"

--- static/js/app.js
+++ static/js/app.js
@@ -36,8 +36,10 @@ function updateFilters() {
     let changedElement = d3.select(this);
     // 4b. Save the value that was changed as a variable.
     let elementValue = changedElement.property("value");
+    console.log(elementValue);
     // 4c. Save the id of the filter that was changed as a variable.
-    let filterId = changedElement("id");
+    let filterId = changedElement.attr("id");
+    console.log(filterId);
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
     if (elementValue) {
@@ -47,7 +49,7 @@ function updateFilters() {
       delete filters[filterId];
     }
     // 6. Call function to apply all filters and rebuild the table
-    console.log(filters);
+    // console.log(filters);
 
     filterTable();
   }
@@ -60,9 +62,9 @@ function updateFilters() {
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    Object.entries(filters).forEach(([key, value]) => {
-      filteredData = filteredData.filter(row => row[key] === value);
-    });
+    Object.entries(filters).forEach(([key,value])=> {
+      filteredData =  filteredData.filter(row => row[key]=== value);
+    }); 
   
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
@@ -71,6 +73,7 @@ function updateFilters() {
   // 2. Attach an event to listen for changes to each filter
   
   d3.selectAll("input").on("change",updateFilters);
+ 
 
   // Build the table when the page loads
   buildTable(tableData);
